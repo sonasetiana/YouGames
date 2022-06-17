3dc8d026be238a7a6d1c7040edd7ad9a62381541
Sona Setiana <sonasetiana13@gmail.com>
2022-06-17 21:30:09 +0700

    Initial Commit

---
+++ YouGames/YouGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouGamesApp.swift
+//  YouGames
+//
+//  Created by sona setiana on 17/06/22.
+//
+
+import SwiftUI
+
+@main
+struct YouGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
